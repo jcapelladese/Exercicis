6c3601523f8de70192aaf40f6bd30f99ca51ea4b
jcapelladese <jordiedo29@gmail.com>
2017-11-17 19:45:47 +0100

    First commit.

---
+++ src/org/escoladeltreball/exercicis/Exercici1.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.escoladeltreball.exercicis;
+
+/**
+ * @author iaw47941842
+ *
+ */
+public class Exercici1 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+}
